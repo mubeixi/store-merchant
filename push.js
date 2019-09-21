2367082d87b43263596fc0fe34c112219092fd06
junxiang wang <function@wzw.com>
2019-09-21 19:06:33 +0800

    feat 集成编译打包自动上线的脚本

---
+++ push.js
@@ -0,0 +1,21 @@
+//局部模式
+var shell = require('shelljs');
+//全局模式下，就不需要用shell开头了。
+//require('shelljs/global');
+
+if (shell.exec('npm run build').code !== 0) {//执行npm run build 命令
+  shell.echo('Error: Git commit failed');
+  shell.exit(1);
+}
+
+
+shell.exec('git pull origin mster');
+
+//由于我的用另外一个仓库存放dist目录，所以这里要将文件增量复制到目标目录。并切换到对应目录。
+shell.cp ('-r', './dist/*', '../shop-diy-dist/htdocs/member/shop/setting/diy');
+shell.cd('../shop-diy-dist');
+
+//不能在一股脑全部添加了
+shell.exec('git add htdocs/member/shop/setting/diy/');
+shell.exec("git commit -m '前端自动编译提交 function'")
+shell.exec('git push origin master')
