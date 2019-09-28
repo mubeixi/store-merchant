3a74337a3923c6206d3cc20e4641f0506b1ebd3d
junxiang wang <function@wzw.com>
2019-09-28 17:48:28 +0800

    fix 右侧组件列表区域，只在开发环境显示

--- src/common/env.ts
+++ src/common/env.ts
@@ -1,5 +1,5 @@
 export const baseApiUrl = 'http://www.qiyeban.com';
-export const apiKey = '458f_$#@$*!fdjisdJDFHUk4%%653154%^@#(FSD#$@0-T';
 
+export const apiKey = '458f_$#@$*!fdjisdJDFHUk4%%653154%^@#(FSD#$@0-T';
 
 export const isDev = process.env.NODE_ENV === 'production'?false:true;
