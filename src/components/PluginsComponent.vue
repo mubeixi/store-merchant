830341a71150a9a5030f8c9c29413a03e9d6f72e
junxiang wang <function@wzw.com>
2019-10-24 11:31:52 +0800

    fix 左侧组件样式优化

--- src/components/PluginsComponent.vue
+++ src/components/PluginsComponent.vue
@@ -5,7 +5,7 @@
           v-for="(item,key) in plugins">
         <div class="center">
           <img draggable="false" class="icon" :src="item.icon"/>
-          <!--          <i data-v-ecfd6a32="" class="iconfont" :class="item.icon"></i>-->
+          <img draggable="false" class="icona" :src="item.icona"/>
           <p class="text">{{item.value}}</p>
         </div>
       </li>
@@ -54,18 +54,33 @@
         height :90px;
         /*border-right 1px solid #f1f1f1
         border-bottom 1px solid #f1f1f1*/
+
         box-sizing :border-box;
         position :relative;
         display :inline-block;
         /*float left*/
         cursor :move;
         &:hover{
-          color:#409EFF;
+
+          background: #409EFF;
 
 
           p{
-            color: #333;
+            color:white;
+            //color: #333;
           }
+
+          .center{
+            /*background: green;*/
+            .icon{
+              display: none
+            }
+            .icona{
+              display: block;
+            }
+          }
+
+
         }
 
 
@@ -77,37 +92,47 @@
           top: 50%;
           transform: translateY(-50%);
           width: 100%;
+          .icona{
+            display: none;
+            width: 26px;
+            height: 26px;
+            /*opacity: 0.8;*/
+            margin: 0 auto 8px;
+            text-align: center;
+            font-size: 25px;
+
+          }
         }
+
+
 
         .icon {
           width: 26px;
           height: 26px;
-          opacity: 0.8;
+          /*opacity: 0.8;*/
           margin: 0 auto 8px;
           display: block;
           text-align: center;
           font-size: 25px;
-          color: #999;
+          //color: #999;
 
-          &:hover {
-            opacity: 1;
-          }
+
         }
 
-        i.iconfont {
-          width: 36px;
-          line-height: 36px;
-          margin: 0 auto;
-          display: block;
-          text-align: center;
-          font-size: 25px;
+        /*i.iconfont {*/
+        /*  width: 36px;*/
+        /*  line-height: 36px;*/
+        /*  margin: 0 auto;*/
+        /*  display: block;*/
+        /*  text-align: center;*/
+        /*  font-size: 25px;*/
 
-          cursor: pointer;
+        /*  cursor: pointer;*/
 
-          &:hover {
+        /*  &:hover {*/
 
-          }
-        }
+        /*  }*/
+        /*}*/
 
         p {
           text-align: center;
