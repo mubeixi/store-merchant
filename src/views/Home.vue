219e518d45461303c4ea1c0ea96f488395bc62df
junxiang wang <function@wzw.com>
2019-10-24 11:42:02 +0800

    fix 顶部新增模拟状态栏

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,24 +7,40 @@
       <div class="plugins">
         <plugins-component></plugins-component>
       </div>
-      <div class="preview">
+      <div class="preview" >
+        <div class="preview-page">
+          <div @click="setAct(0,'system','全局设置','针对页面的整体配置')" :class="{active:mode==='system'}" class="item preview-page-options"><i class="preview-page-options-icon"></i>全局设置</div>
+          <div @click="setAct(1,'plugin','组件管理','可以便捷拖动、删除组件')" :class="{active:mode==='plugin'}" class="item preview-page-coms"><i class="preview-page-coms-icon"></i>组件管理</div>
+<!--          <div @click="saveData" :class="{active:previewActiveIndex===2}" class="item preview-page-save"><i class="preview-page-coms-icon"></i>数据保存</div>-->
+<!--          <div :class="{active:previewActiveIndex===3}" class="item preview-page-more"><i class="preview-page-coms-icon"></i>更多操作</div>-->
+        </div>
         <preview-component ref="preview" @setData="setDataEv"></preview-component>
       </div>
       <div class="setattr">
-        <set-attr-component ref="setAttr"></set-attr-component>
+        <div class="deco-component-title">
+          <div class="deco-component-title__header"><span class="deco-component-title__name">{{componentTitle.title}}</span></div>
+          <div class="deco-component-title__msg">{{componentTitle.desc}}</div>
+        </div>
+        <set-attr-component v-show="mode=='attr'" ref="setAttr"></set-attr-component>
+        <right-component  v-show="mode=='plugin'"></right-component>
       </div>
     </div>
-
-    <div class="right">
-      <right-component></right-component>
+    <div class="handle">
+      <el-button @click="saveData" type="primary" size="small">保存</el-button>
+<!--      <el-button size="small">预览</el-button>-->
+<!--      <el-button size="small">更多操作</el-button>-->
     </div>
+
+<!--    <div class="right">-->
+<!--      -->
+<!--    </div>-->
 
   </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
-    import {mapState} from 'vuex';
+    import {mapState,mapActions} from 'vuex';
     import SetAttrComponent from '@/components/SetAttrComponent.vue'; // @ is an alias to /src
     import PreviewComponent from '@/components/PreviewComponent.vue';
     import PluginsComponent from '@/components/PluginsComponent.vue';
@@ -43,17 +59,29 @@
         },
         data() {
             return {
-                isDev: isDev
+                isDev: isDev,
+                previewActiveIndex:null
             }
         },
         methods: {
+            ...mapActions(['setMode','setComponentTitle']),
+            setAct(idx,mode,title,desc){
+                this.previewActiveIndex=idx;
+                this.setMode(mode);
+                this.setComponentTitle({title,desc})
+            },
             // 这个数据一直往上传，这么辛苦
             setDataEv(data) {
                 // this.activeObj = data
             },
+            saveData(){
+                // this.previewActiveIndex=2;
+                this.$refs.preview.uploadConfig();
+
+            }
         },
         computed: {
-            ...mapState(['activeAttr', 'editStatus']),
+            ...mapState(['activeAttr', 'editStatus','mode','componentTitle']),
         },
         mounted() {
             // 右侧如果内容过多，可以用滚动栏
@@ -81,6 +109,17 @@
   /*  box-sizing border-box*/
   /*  text-align right*/
   /*  border-bottom 1px solid #e7e7e7*/
+  .handle
+    position fixed
+    z-index 999
+    bottom 0
+    width 100%
+    height 50px
+    display flex
+    align-items center
+    background white
+    justify-content center
+    border-top 1px solid #e7e7e7
 
   .wrap
     padding-top 0px
@@ -93,17 +132,25 @@
 
     .preview
       position relative
-      top 70px
-      position: relative;
-      min-height: 601px;
-      width: 1549px;
+      flex 1
+      //height calc(100vh - 70px)
+      height 100vh
+      /*box-sizing border-box*/
+      /*padding-top 70px*/
+      overflow-x hidden
+      overflow-y auto
+      background #f7f8fa
+      //min-height: 601px;
+
 
     .setattr
-      height calc(100vh - 200px)
+      height calc(100vh - 70px)
       /*overflow-y scroll*/
+      padding-bottom 70px
       overflow-x hidden
-      width 540px
-      padding-right 20px
+      width 460px
+      /*width 376px*/
+      /*padding-right 20px*/
 
   .right
     position fixed
@@ -122,4 +169,75 @@
   //  width:0px;
   //  height:0px;
   //}
+</style>
+<style lang="less">
+.preview::-webkit-scrollbar {
+  display: none;
+}
+
+.setattr::-webkit-scrollbar {
+  display: none;
+}
+
+#preview{
+  /*overflow-x:hidden;*/
+  /*overflow-y :auto;*/
+  /*position: absolute;*/
+  /*top: 0;*/
+  /*bottom: 0;*/
+  /*width: 100%;*/
+}
+
+.preview-page{
+  position: absolute;
+  top: 70px;
+  right: 10px;
+  text-align: center;
+  font-size: 12px;
+  color: #323233;
+  z-index: 1;
+  .item{
+    background-color: #fff;
+    box-shadow: 0 2px 8px 0 rgba(0,0,0,.1);
+    border-radius: 2px;
+    width: 94px;
+    height: 32px;
+    line-height: 32px;
+    margin-bottom: 12px;
+    cursor: pointer;
+    &.active{
+      background-color: #66b1ff;
+      color: #fff;
+    }
+    &.preview-page-save:hover{
+      background-color: #66b1ff;
+      color: #fff;
+    }
+    &.preview-page-more:hover{
+      background-color: #66b1ff;
+      color: #fff;
+    }
+  }
+}
+
+
+.deco-component-title{
+  padding: 24px 16px;
+  border-bottom: 1px solid #f2f4f6;
+}
+
+.deco-component-title__name {
+  margin-right: auto;
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 24px;
+  color: #323233;
+  text-align: left;
+}
+.deco-component-title__msg {
+  margin-top: 12px;
+  font-size: 14px;
+  line-height: 20px;
+  color: #969799;
+}
 </style>
