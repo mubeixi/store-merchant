0aa74a32ae86adbbe66ca9c82f74ac7486588614
jiahaihong <527538629@qq.com>
2019-12-24 11:35:17 +0800

    增加了选中样式

--- src/common/mixin.ts
+++ src/common/mixin.ts
@@ -16,7 +16,7 @@ export const doLoginMixin = {
       Cookies.set('Users_ID', 'wkbq6nc2kc')
       Cookies.set('Stores_Bind_User_ID', '48')//为了区分其他的user_id，所以弄了这个代表店铺的user_id
       Cookies.set('Stores_ID', '10')
-      Cookies.set('access_token', 'ZTU81QlYkPC0SvWKYVkOw2YXP7ytbTE7')
+      Cookies.set('access_token', 'ZLoLIzcPPkWMPQkjuZhpOZ7rto2n09Cr')
     }
 
     if(!Cookies.get('Users_ID')){
