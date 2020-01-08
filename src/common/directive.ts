8954642aace02f0db9c3e238c527ebebb3cbcd0c
junxiang wang <function@wzw.com>
2020-01-08 11:15:00 +0800

    fix directive

--- src/common/directive.ts
+++ src/common/directive.ts
@@ -1,16 +1,12 @@
 import Vue from 'vue';
-import {ls} from "./tool/ls";
 
-// window.preScrollTop = 0;
-
-// Vue.directive('scroll', {
-//   // 当绑定元素插入到 DOM 中
-//   inserted: function (el,binding) {
-//     var cb = binding.value
-//     el.addEventListener('mousewheel',function(e){
-//       // var direction = e.deltaY>0?'down':'up'
-//       window.preScrollTop += e.deltaY
-//       // cb(e)
+// // 注册一个全局自定义指令 `wzw-finder`唤起资源管理器
+// Vue.directive('wzw-finder', {
+//   // 当被绑定的元素插入到 DOM 中时……
+//   bind: function (el,binding, vnode, oldVnode) {
+//     // 点击唤起文件上传
+//     el.addEventListener('click',function(){
+//       console.log(el, binding.value)
 //     })
 //   }
 // })
